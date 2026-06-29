ad99cce8a3057016971f7053ce3a4305e8ff5d0e
aur2 <141899205+aur2@users.noreply.github.com>
2026-06-29 15:44:51 +0800

    更新 index.php

--- assets/qs/index.php
+++ assets/qs/index.php
@@ -1,13 +1,17 @@
-<?php
-$version="2.0";
-$updateInfo="• 新增 在线脚本";
-$zipUrl="https://qqbyh.cn/assets/Serendipity/app.zip";
-
-header("Content-Type:application/json;charset=utf-8");
-header("Access-Control-Allow-Origin:*");
-echo json_encode([
-"bb"=>$version,
-"nr"=>$updateInfo,
-"url"=>$zipUrl
-],JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
-?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta charset="UTF-8">
+  <title>JSON展示页面</title>
+</head>
+  <body>
+<script>
+const data={
+"bb": "1.0",
+"nr": "更新啦",
+"url": "https://qq.com"
+};
+document.body.innerText=JSON.stringify(data, null, 2);
+</script>
+</body>
+</html>
